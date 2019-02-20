13f242994c056e3261d2e1baf044dd9abd145668
shravs1 <shravankumar.nagarajan2@cognizant.com>
2019-02-21 03:25:49 +0530

    Added initial Angular Menu

--- Volunteer-Marathon-UI/src/app/app.module.ts
+++ Volunteer-Marathon-UI/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatMenuModule, MatButtonModule, MatIconModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
